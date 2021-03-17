2ae0895260168c353c7808c0c5b0818a6dd41aba
SandeepWork <sandeep@tethra.io>
2021-03-17 13:09:44 -0400

    initial commit

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:sixthapp/screens/background_painter.dart';
 
+import 'hello.dart';
+
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key key}) : super(key: key);
 
@@ -30,22 +32,45 @@ class _AuthScreenState extends State<AuthScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Donate'),
+      ),
       body: Stack(
         children: [
           SizedBox.expand(
-              child: CustomPaint(
-            painter: BackgroundPainter(
-              animation: _controller.view,
+            child: CustomPaint(
+              painter: BackgroundPainter(
+                animation: _controller.view,
+              ),
             ),
-          )),
-          Center(
-            child: RaisedButton(
-              onPressed: () {
-                _controller.forward(from: 0);
-              },
-              child: Text('Animate'),
+          ),
+          Container(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RaisedButton(
+                  onPressed: () {
+                    _controller.forward(from: 0);
+                  },
+                  child: Text('Animate'),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    /// [Navigator.push brings new screen over the app, HelloScreen in this case]
+                    /// [Hello Screen is made in hello.dart file inside screens folder]
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (_) => HelloScreen(),
+                      ),
+                    );
+                  },
+                  child: Text('Route to another Screen'),
+                ),
+              ],
             ),
-          )
+          ),
         ],
       ),
     );
--- lib/screens/hello.dart
+++ lib/screens/hello.dart
@@ -1,1 +1,15 @@
-//hello
+import 'package:flutter/material.dart';
+
+class HelloScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Piegon App'),
+      ),
+      body: Center(
+        child: Text('Hello Screenlass  HelloScreen'),
+      ),
+    );
+  }
+}
